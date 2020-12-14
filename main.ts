9303d04515a33b54d29c3be9eb6392c5c4d0d39f
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2020-12-14 23:22:43 +0100

    Remote control

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+function Left (vitesse: number) {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, vitesse, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, vitesse, 67)
+}
+function Recule (vitesse: number) {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, vitesse, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, vitesse, 67)
+}
+function ping () {
+	
+}
 function Avance (vitesse: number) {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, vitesse, 67)
@@ -11,20 +26,53 @@ function Arrete () {
     PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 0, 67)
 }
 maqueen.IR_callbackUser(function ({ myparam: message }) {
-    // Si on a appuyé sur OK
+    modif_time = control.millis()
     if (message == 64) {
         EnMarche = 1
+        strip.showRainbow(1, 360)
     } else if (message == 66) {
         EnMarche = 0
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (message == 70) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        motion = 1
+    } else if (message == 68) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        motion = 2
+    } else if (message == 67) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        motion = 3
+    } else if (message == 21) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        motion = 4
+    } else if (message == 22) {
+        vitesse = 20
+    } else if (message == 25) {
+        vitesse = 50
+    } else if (message == 13) {
+        vitesse = 75
+    } else if (message == 12) {
+        vitesse = 100
     } else {
     	
     }
+    message = 0
 })
+function Right (vitesse: number) {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, vitesse, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, vitesse, 67)
+}
 let DR = 0
 let DL = 0
 let PR = 0
 let PL = 0
+let motion = 0
 let EnMarche = 0
+let modif_time = 0
+let vitesse = 0
+let strip: neopixel.Strip = null
 serial.redirectToUSB()
 PCA9685.reset(67)
 PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED5, 100, 67)
@@ -34,10 +82,10 @@ pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
-EnMarche = 0
-let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
 strip.clear()
 strip.showColor(neopixel.colors(NeoPixelColors.Red))
+vitesse = 20
 basic.forever(function () {
     if (EnMarche == 1) {
         let distance = 0
@@ -55,6 +103,19 @@ basic.forever(function () {
             Arrete()
         }
     } else {
-        Arrete()
+        if (motion == 1) {
+            Avance(vitesse)
+        } else if (motion == 2) {
+            Left(vitesse)
+        } else if (motion == 3) {
+            Right(vitesse)
+        } else if (motion == 4) {
+            Recule(vitesse)
+        } else {
+            Arrete()
+        }
+        if (control.millis() - modif_time > 500) {
+            motion = 0
+        }
     }
 })
